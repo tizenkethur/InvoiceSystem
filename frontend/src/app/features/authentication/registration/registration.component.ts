d4b66ba1c7c4707f465f1de04a51a5221283af7b
tizenkethur <vivientalos29@gmail.com>
2023-04-29 14:04:32 +0200

    angular material added, registration form created with validation and style

--- frontend/src/app/features/authentication/registration/registration.component.ts
+++ frontend/src/app/features/authentication/registration/registration.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent {
+  form = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
+    ]),
+  });
 
+  get username(): AbstractControl {
+    return this.form.get('username');
+  }
+
+  get password(): AbstractControl {
+    return this.form.get('password');
+  }
+
+  get email(): AbstractControl {
+    return this.form.get('email');
+  }
+
+  register(): void {
+    // if (this.form.valid) {
+    //   const user: UserRegistrationRequestModel = this.form.getRawValue();
+    //   this.authenticationService.register(user).subscribe();
+    // }
+  }
 }
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
-  exports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class SharedModule {}
