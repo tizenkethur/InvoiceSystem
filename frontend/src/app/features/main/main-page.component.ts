cf7cf8b46b316063e3aa1118dbcdc652b243648c
tizenkethur <vivientalos29@gmail.com>
2023-05-03 14:43:49 +0200

    buttons in menu displayed regarding roles

--- frontend/src/app/features/main/main-page.component.ts
+++ frontend/src/app/features/main/main-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../../core/services/AuthService/auth-service.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  roleTypeIdObservable$ = this.authService.roleTypeIdObservable$;
+  currentRoleTypeId: number;
 
+  ngOnInit() {
+    this.roleTypeIdObservable$.subscribe((response) => {
+      this.currentRoleTypeId = parseInt(response);
+    });
+  }
+  constructor(private authService: AuthService) {}
 }
